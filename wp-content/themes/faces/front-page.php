d6a24e894111beea3f29324524c2613a09ec4091
WEBzaytsev <studio@webzaytsev.ru>
2022-04-25 02:10:08 +0200

    add first section

--- wp-content/themes/faces/front-page.php
+++ wp-content/themes/faces/front-page.php
@@ -1,5 +1,36 @@
 <?php get_header(); ?>
 
+<main class="home-page">
+    <div class="container pos-r first-section">
+        <h1 class="uppercase page-title">
+            <span class="pos-r page-title_s">Работаем
+                <span class="pos-a"><?php get_template_part('/vector-images/page-title-s'); ?></span>
+            </span> блогерами
+            <span class="page-title_bottom">и лидерами мнений</span></h1>
 
+        <div class="pos-r full-width">
+            <div class="mx-auto pos-r first-section__circle">
+                <div class="hidden border50 pos-a first-section__circle_wrap">
+                    <div class="first-section__circle_image"></div>
+                </div>
+                <?php get_template_part('/vector-images/round-text'); ?>
+            </div>
+
+            <p class="pos-a first-section__left-top-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Porta non a, mi ut in rutrum.</p>
+
+            <div class="pos-a border50 flex-center round-animate-btn">
+                <span class="border50 pointer text-center flex-center uppercase">обсудить проект</span>
+            </div>
+            <a href="#" class="pos-a uppercase text-center flex-center transition border50 first-section__cases">наши <br>кейсы
+                <span class="pos-a first-section__cases_arr">
+                    <?php get_template_part('/vector-images/cases-arr'); ?>
+                </span>
+            </a>
+        </div>
+
+        <p class="first-section__bottom-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Porta non a, mi ut in rutrum. Pretium justo, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Porta non a, mi ut in rutrum. Pretium justo,</p>
+
+    </div>
+</main>
 
 <?php get_footer(); ?>
--- wp-content/themes/faces/functions.php
+++ wp-content/themes/faces/functions.php
@@ -141,6 +141,7 @@ function faces_scripts() {
 	wp_enqueue_style( 'faces-style', get_stylesheet_uri(), array(), _FACES_VERSION );
 	wp_enqueue_style( 'main-style', get_template_directory_uri() . '/dist/css/main.css', array(), _FACES_VERSION );
     wp_enqueue_style( 'font-manrope', 'https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap', array(), _FACES_VERSION );
+    wp_enqueue_style( 'font-tenor', 'https://fonts.googleapis.com/css2?family=Tenor+Sans&display=swap', array(), _FACES_VERSION );
 	wp_style_add_data( 'faces-style', 'rtl', 'replace' );
 
 	wp_enqueue_script( 'faces-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _FACES_VERSION, true );
