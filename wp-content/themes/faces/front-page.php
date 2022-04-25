545d1d7d31f3b8de721b63758e2132fa64080d67
WEBzaytsev <studio@webzaytsev.ru>
2022-04-25 23:01:02 +0200

    add acf

--- wp-content/themes/faces/front-page.php
+++ wp-content/themes/faces/front-page.php
@@ -2,11 +2,7 @@
 
 <main class="home-page">
     <div class="container pos-r first-section">
-        <h1 class="uppercase page-title">
-            <span class="pos-r page-title_s">Работаем
-                <span class="pos-a"><?php get_template_part('/vector-images/page-title-s'); ?></span>
-            </span> блогерами
-            <span class="page-title_bottom">и лидерами мнений</span></h1>
+        <h1 class="uppercase page-title"><?php the_field('page-caption'); ?></h1>
 
         <div class="pos-r full-width">
             <div class="mx-auto pos-r first-section__circle">
@@ -16,7 +12,7 @@
                 <?php get_template_part('/vector-images/round-text'); ?>
             </div>
 
-            <p class="pos-a first-section__left-top-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Porta non a, mi ut in rutrum.</p>
+            <p class="pos-a first-section__left-top-text"><?php the_field('text-top-right'); ?></p>
 
             <div class="pos-a border50 flex-center round-animate-btn">
                 <span class="border50 pointer text-center flex-center uppercase">обсудить проект</span>
@@ -28,7 +24,7 @@
             </a>
         </div>
 
-        <p class="first-section__bottom-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Porta non a, mi ut in rutrum. Pretium justo, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Porta non a, mi ut in rutrum. Pretium justo,</p>
+        <p class="first-section__bottom-text"><?php the_field('text-bottom-left'); ?></p>
 
     </div>
 
