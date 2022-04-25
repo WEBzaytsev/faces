1236ceaf05ac707ea96cab9c54d9ca4d05d8a233
WEBzaytsev <studio@webzaytsev.ru>
2022-04-25 19:14:01 +0200

    add options panel

--- wp-content/themes/faces/functions.php
+++ wp-content/themes/faces/functions.php
@@ -185,6 +185,20 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 }
 
 /**
+ * Set options panel
+ */
+if( function_exists('acf_add_options_page') ) {
+
+    acf_add_options_page(array(
+        'page_title' 	=> 'Theme General Settings',
+        'menu_title'	=> 'General Settings',
+        'menu_slug' 	=> 'theme-general-settings',
+        'capability'	=> 'edit_posts',
+        'redirect'		=> false
+    ));
+}
+
+/**
  * Create custom post types
  */
 require get_template_directory() . '/includes/custom-post-type.php';
