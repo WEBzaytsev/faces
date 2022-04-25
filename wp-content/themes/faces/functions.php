62df041adb5dfe66559bba702397a4337d764092
WEBzaytsev <studio@webzaytsev.ru>
2022-04-26 01:55:31 +0200

    add menus

--- wp-content/themes/faces/functions.php
+++ wp-content/themes/faces/functions.php
@@ -49,7 +49,8 @@ function faces_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', 'faces' ),
+			'footer-menu' => esc_html__( 'footer', 'faces' ),
+            'header-menu' => esc_html__('header', 'faces'),
 		)
 	);
 
