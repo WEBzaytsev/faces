8788f31d8871370d32ae5692eadfdf6a80448734
WEBzaytsev <studio@webzaytsev.ru>
2022-05-05 21:16:59 +0200

    update case modal

--- wp-content/themes/faces/template-parts/case-modal.php
+++ wp-content/themes/faces/template-parts/case-modal.php
@@ -14,5 +14,35 @@ $img_url = get_field('modal-img', $case_id)['url']
         <?php esc_html_e(get_field('description', $case_id), 'faces'); ?>
     </p>
 
-        Ultricies lorem id viverra euismod sit lectus. Eu blandit eget tincidunt pharetra bibendum. Pharetra, interdum ut felis dictumst praesent. Velit libero imperdiet mi id nunc, ut facilisis placerat sed. Faucibus aenean turpis nisi, euismod.</p>
+    <?php if (isset($img_url)) : ?>
+    <div class="full-width pos-r z-1 hidden border-70 modal-window__case-img"
+         style="background-image: url('<?php esc_attr_e($img_url, 'faces'); ?>');"></div>
+    <?php endif; ?>
+
+    <?php if (isset($stats)) : ?>
+        <div class="pos-r z-2 full-width flex align-start justify-between stats">
+            <?php foreach ($stats as $stat) : ?>
+            <div class="full-width stats__item">
+                <div class="flex-center border50 bg-white mx-auto stats__item_num">
+                    <span class="color-primary text-62 line-height-73 font-tenor">
+                        <?php esc_html_e(sprintf('%g%%', $stat['value']), 'faces'); ?>
+                    </span>
+                </div>
+                <p class="text-center text-20 line-height-27 stats__item_desc">
+                    <?php esc_html_e($stat['desc'], 'faces'); ?>
+                </p>
+            </div>
+            <?php endforeach; ?>
+        </div>
+    <?php endif; ?>
+
+    <p class="text-60 text-center font-tenor line-height-70 modal-window__title">
+        <?php esc_html_e('Хочу также', 'faces'); ?>
+    </p>
+    <p class="text-center text-20">
+        <?php esc_html_e('Оставьте заявку и мы свяжемся с вами', 'faces'); ?>
+    </p>
+    <?php get_template_part('/template-parts/feedback-form', null, array(
+        'type' => 'case',
+    )); ?>
 </div>
