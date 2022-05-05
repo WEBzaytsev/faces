3c833adcf033f2d3c61d9cb32f9d79aa3fa2e8fe
WEBzaytsev <studio@webzaytsev.ru>
2022-05-06 00:09:16 +0200

    cases mobile

--- wp-content/themes/faces/template-parts/work-offer-btn.php
+++ wp-content/themes/faces/template-parts/work-offer-btn.php
@@ -5,7 +5,7 @@ $classes = sprintf('border50 pointer text-center flex-center font-tenor uppercas
 $btn_text = $args['text'];
 ?>
 
-<div class="pos-a z-1 border50 flex-center round-animate-btn"
+<div class="pos-a z-2 border50 flex-center round-animate-btn"
      data-modal="work-offer">
     <span class="<?php esc_attr_e($classes, 'faces'); ?>"><?php echo __($btn_text, 'faces'); ?></span>
 </div>
