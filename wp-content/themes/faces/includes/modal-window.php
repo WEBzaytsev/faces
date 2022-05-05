83284536d2a014453be43991fcd437afa9a2326b
WEBzaytsev <studio@webzaytsev.ru>
2022-05-05 21:16:03 +0200

    pass case ID to case modal

--- wp-content/themes/faces/includes/modal-window.php
+++ wp-content/themes/faces/includes/modal-window.php
@@ -1,12 +1,17 @@
 <?php
 
 function ajax_modal() {
-    $current_modal = $_POST['modal'];
+    $current_modal = $_POST['modal'] ?? null;
+    $case_id = $_POST['case_'] ?? null;
 
     if ($current_modal == 'work-offer') {
         get_template_part('/template-parts/work-offer-modal');
-    } elseif ($current_modal == 'case') {
-        get_template_part('/template-parts/case-modal');
+    } elseif ($current_modal == 'case' && $case_id) {
+        get_template_part('/template-parts/case-modal', null, array(
+            'id' => $case_id,
+        ));
+    } else {
+        echo 0;
     }
     die();
 }
