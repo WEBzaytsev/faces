4f75585b4dec884181161020493ff71e044803b6
WEBzaytsev <studio@webzaytsev.ru>
2022-05-06 00:21:40 +0200

    separate filters to single class

---
+++ wp-content/themes/faces/app/js/pages/filters.js
@@ -0,0 +1,98 @@
+'use strict';
+
+import {modalWindow} from "../modalWindow";
+
+export const FiltersClass = function ($, settings, parent, action) {
+    const self = this;
+    this.mainSettings = settings;
+    this.filters = $('.filters__item');
+    this.activeFilter = $('.filters__item.active');
+    this.blockFilters = false;
+    this.contentWrap = $('.posts-content');
+
+    this.filterHandler = function () {
+
+        if (self.blockFilters) {
+            return;
+        }
+
+        if ($(this).hasClass('active')) {
+            return;
+        }
+
+        const currentCat = $(this).data('cat') || 'all';
+
+        self.switchActiveFilter($(this));
+        self.getCases(currentCat);
+        self.setCatUrl(currentCat);
+    }
+
+    this.setCatUrl = (cat) => {
+        const url = new URL(window.location);
+        url.searchParams.set('cat', cat);
+        window.history.pushState(null, document.title, url);
+    }
+
+    this.switchActiveFilter = (elem) => {
+        self.activeFilter.removeClass('active');
+        self.activeFilter = elem;
+        self.activeFilter.addClass('active');
+    }
+
+    this.getCases = (cat) => {
+        $.ajax({
+            url : self.mainSettings.ajax_url,
+            data : {
+                action: action,
+                cat: cat || 'all',
+            },
+            type : 'POST',
+            beforeSend : function( xhr ){
+                self.blockFilters = true;
+                self.contentWrap.html(`<div class="loader active">
+                            <div class="loader-inner">
+                                <div class="loader-animate">
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                            </div>
+                        </div>`)
+            },
+            success : function( data ) {
+                if (data) {
+                    self.contentWrap.html(data);
+                    if (typeof parent.modals === 'function') {
+                        parent.modals();
+                    }
+                }
+                self.blockFilters = false;
+            }
+        });
+    }
+
+    this.modals = () => {
+        const modalBtns = $('[data-modal="case"]');
+
+        if (modalBtns.length) {
+            modalBtns.each(function () {
+                const modals = new modalWindow($, settings, $(this));
+                modals.init();
+            })
+        }
+    }
+
+    this.init = () => {
+        this.filters.each(function () {
+            $(this).on('click', self.filterHandler);
+        })
+    }
+}
