981c363dc930a0cadc6333ee73fbc2c6b1104be9
WEBzaytsev <studio@webzaytsev.ru>
2022-05-16 18:21:31 +0200

    edit work offer button

--- wp-content/themes/faces/template-parts/work-offer-btn.php
+++ wp-content/themes/faces/template-parts/work-offer-btn.php
@@ -5,7 +5,7 @@ $classes = sprintf('border50 pointer text-center flex-center font-tenor uppercas
 $btn_text = $args['text'];
 ?>
 
-<div class="pos-a z-2 border50 flex-center sm-text-12 sm-line-height144 round-animate-btn"
+<div class="pos-a z-2 border50 flex-center text-26 large-text-16 sm-text-12 line-height144 round-animate-btn"
      data-modal="work-offer">
     <span class="<?php esc_attr_e($classes, 'faces'); ?>"><?php echo __($btn_text, 'faces'); ?></span>
 </div>
