4ffb94bd9a912890f3074730c331e1d2e03eb679
WEBzaytsev <studio@webzaytsev.ru>
2022-05-19 14:35:15 +0300

    pot

--- wp-content/themes/faces/template-parts/work-offer-modal.php
+++ wp-content/themes/faces/template-parts/work-offer-modal.php
@@ -4,7 +4,7 @@
         <?php esc_html_e('Давайте поработаем', 'faces'); ?>
     </p>
     <p class="text-center text-20 md-text-14 md-line-height144 modal-window__text">
-        <?php esc_html_e('Для дальнейшй совместной работы нам необходима минимальная
+        <?php esc_html_e('Для дальнейшей совместной работы нам необходима минимальная
         информация', 'faces'); ?>
     </p>
     <?php get_template_part('/template-parts/feedback-form', null, array(
