bb8796df48435647c8bce841e06b29175f246c1f
WEBzaytsev <studio@webzaytsev.ru>
2022-06-09 18:15:28 +0200

    fix displaying categories images in posts loop

--- wp-content/themes/faces/template-parts/cases-content.php
+++ wp-content/themes/faces/template-parts/cases-content.php
@@ -20,18 +20,23 @@ if ($category != '') {
         'slug' => $category,
     ]);
 }
+
+$category_id = null;
 ?>
 
 <?php if (have_posts()) : ?>
-    <?php while (have_posts()) : the_post();
+    <?php while (have_posts()) :
+        the_post();
 
         $post_id = $post->ID;
-        $category_id = null;
+
         if (isset($current_category)) {
             $category_id = $current_category[0]->term_id;
         } else {
-            $current_cat = get_the_terms($post, 'cases_cat');
-            $category_id = $current_cat[0]->term_id;
+            $current_cat = get_the_terms($post_id, 'cases_cat');
+            if ($current_cat) {
+                $category_id = $current_cat[0]->term_id;
+            }
         }
 
         $category_img_url = get_field('img', 'cases_cat_' . $category_id)['url']; ?>
@@ -40,7 +45,7 @@ if ($category != '') {
              data-modal="case" data-case="<?php esc_attr_e($post_id, 'faces'); ?>">
             <div class="pos-a z-2 case__cat">
                 <div class="pos-r z-2 full-width h-100 case__cat_img"
-                     style="background-image:url('<?php esc_attr_e($category_img_url, 'faces'); ?>');"></div>
+                     style="background-image:url('<?php echo esc_url($category_img_url, 'faces'); ?>');"></div>
             </div>
             <figure class="hidden flex-center pos-r z-1 case__img"
                     style="background-image: url('<?php esc_attr_e(get_field('img')['url'], 'faces'); ?>');">
@@ -53,7 +58,8 @@ if ($category != '') {
                 <?php echo __(get_field('description')); ?>
             </p>
         </div>
-    <?php endwhile; ?>
+    <?php endwhile;
+    wp_reset_postdata(); ?>
     <?php if (count($posts) == 6) : ?>
         <div class="full-width">
             <a href="#"
